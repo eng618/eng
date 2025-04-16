8ac1c90d5daf12de0f4b0c8eabd49423f2974798
Eric N. Garcia <eng618@garciaericn.com>
2025-04-16 01:24:05 -0400

    feat: add system update logic including brew

--- cmd/system/update_system.go
+++ cmd/system/update_system.go
@@ -20,16 +20,54 @@ var UpdateSystemCmd = &cobra.Command{
 			return
 		}
 
-		if strings.Contains(strings.ToLower(string(output)), "ubuntu") {
-			log.Message("Running system update for Ubuntu...")
-			updateCmd := exec.Command("sudo", "apt", "update", "&&", "sudo", "apt", "upgrade", "-y")
-			if err := updateCmd.Run(); err != nil {
-				log.Error("Error updating system: %s", err)
-			} else {
-				log.Message("System updated successfully.")
-			}
+		uname := strings.ToLower(string(output))
+		if strings.Contains(uname, "ubuntu") {
+			updateUbuntu()
+		} else if strings.Contains(uname, "darwin") {
+			updateMacOS()
+		} else if strings.Contains(uname, "raspberrypi") || strings.Contains(uname, "raspbian") {
+			updateRaspberryPi()
 		} else {
-			log.Message("This system is not yet supported for updates.")
+			log.Warn("This system is not yet supported for updates.")
 		}
 	},
+}
+
+func updateUbuntu() {
+	log.Message("Running system update for Ubuntu...")
+	log.Message("About to run a command with sudo. You may be prompted for your system password.")
+	updateCmd := exec.Command("sudo", "apt", "update", "&&", "sudo", "apt", "upgrade", "-y")
+	if err := updateCmd.Run(); err != nil {
+		log.Error("Error updating system: %s", err)
+	} else {
+		log.Message("System updated successfully.")
+	}
+	updateBrew()
+}
+
+func updateMacOS() {
+	log.Message("System update for macOS is coming soon.")
+	updateBrew()
+}
+
+func updateRaspberryPi() {
+	log.Message("System update for Raspberry Pi is coming soon.")
+	updateBrew()
+}
+
+func updateBrew() {
+	_, err := exec.LookPath("brew")
+	if err != nil {
+		log.Message("Homebrew (brew) is not installed on this system.")
+		return
+	}
+	log.Message("Running Homebrew update and upgrade...")
+	updateCmd := exec.Command("bash", "-c", "brew update && brew outdated && brew upgrade")
+	updateCmd.Stdout = log.Writer()
+	updateCmd.Stderr = log.ErrorWriter()
+	if err := updateCmd.Run(); err != nil {
+		log.Error("Error updating Homebrew packages: %s", err)
+	} else {
+		log.Message("Homebrew packages updated successfully.")
+	}
 }
