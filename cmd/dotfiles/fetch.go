9419f3c679c449f0e6247daf6618de362cffbcf2
Eric N. Garcia <eng618@garciaericn.com>
2025-04-22 02:12:58 -0400

    fix: correct typo in fetch command description and enhance environment variable handling

--- cmd/dotfiles/fetch.go
+++ cmd/dotfiles/fetch.go
@@ -1,6 +1,8 @@
 package dotfiles
 
 import (
+	"os"
+
 	"github.com/eng618/eng/utils/log"
 	"github.com/eng618/eng/utils/repo"
 	"github.com/spf13/cobra"
@@ -9,18 +11,18 @@ import (
 
 var FetchCmd = &cobra.Command{
 	Use:   "fetch",
-	Short: "fetch your local bear repository",
+	Short: "fetch your local bare repository",
 	Long:  `This command fetches remote changes to the local bare dot repository.`,
 	Run: func(cmd *cobra.Command, args []string) {
 		log.Start("Fetching dotfiles")
 
-		repoPath := viper.GetString("dotfiles.repoPath")
+		repoPath := os.ExpandEnv(viper.GetString("dotfiles.repoPath"))
 		if repoPath == "" {
 			log.Error("dotfiles.repoPath is not set in the configuration file")
 			return
 		}
 
-		worktreePath := viper.GetString("dotfiles.worktree")
+		worktreePath := os.ExpandEnv(viper.GetString("dotfiles.worktree"))
 		if worktreePath == "" {
 			log.Error("dotfiles.worktree is not set in the configuration file")
 			return
