7db17c0565b7405ab55e39024a7bf67cdebe2324
Eric N. Garcia <eng618@garciaericn.com>
2026-02-05 16:45:21 -0500

    fix(lint): add prettier-plugin-organize-imports dependency

--- cmd/codemod/lint_setup.go
+++ cmd/codemod/lint_setup.go
@@ -119,6 +119,7 @@ func installLintDependencies(echo bool) error {
 		"eslint-config-prettier@latest", "eslint-plugin-prettier@latest",
 		"@eng618/prettier-config@latest", "globals@latest",
 		"husky@latest", "lint-staged@latest", "prettier@latest",
+		"prettier-plugin-organize-imports@latest",
 	}
 
 	// Add TypeScript ESLint dependencies only if TypeScript is detected
